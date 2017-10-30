addd18409b8a633ed985b9f6f4ebd75fe32c0f0d
Anish Thite <anishthite@live.com>
2017-10-30 00:35:51 -0400

    x

--- histodata.py
+++ histodata.py
@@ -6,10 +6,10 @@ import pandas_datareader.data as web
 
 
 style.use('ggplot')
-start = dt.datetime(2017,1,1)
-end = dt.datetime(2017,6,27)
+start = dt.datetime(2017,10,1)
+end = dt.datetime(2017,10,25)
 
-df = web.DataReader('TSLA','google',start,end,200)
+df = web.DataReader('AMZN','yahoo',start,end)
 
-df[['High','Low','Open','Close']].plot()
+df[['High','Low','Open','Close','Volume','Adj Close']].plot()
 plt.show()

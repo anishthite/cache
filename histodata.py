8f5d4e80ae7238f2436b6c30d1367d01bd6185e6
Anish Thite <anishthite@live.com>
2018-09-23 17:19:00 -0400

    real

--- histodata.py
+++ histodata.py
@@ -6,10 +6,16 @@ import pandas_datareader.data as web
 
 
 style.use('ggplot')
-start = dt.datetime(2017,10,1)
+start = dt.datetime(2000,10,1)
 end = dt.datetime(2017,10,25)
 
-df = web.DataReader('AMZN','yahoo',start,end)
+df = web.DataReader('GOOG','yahoo',start,end)
 
-df[['High','Low','Open','Close','Volume','Adj Close']].plot()
-plt.show()
+#print(df.index.day)
+above1050 = df[ df['Open'] >970]
+
+# above1050['Open'].plot()
+#df[['Open','Close','Volume']].plot()
+
+
+
--- realtimedata.py
+++ realtimedata.py
@@ -1,3 +1,13 @@
-from googlefinance import getQuotes
-import json
-print(json.dumps(getQuotes('AAPL'), indent=2))
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+import time, os
+ts = TimeSeries(key='LE8ABWBMWK8PZ0T4', output_format='pandas', indexing_type='date')
+checktime = time.time() + 12
+
+symbol = os.sys.argv[1]
+
+while True:
+    data, meta_data = ts.get_intraday(symbol=symbol.upper(),interval='1min', outputsize='full')
+    print(data.tail(1))
+    # if data['Volume'][-1] > data['Volume'][]
+    time.sleep(60)
